d55dad1afcc8a67fe62fde786ff629cf7c5fe20e
heroniewold.github.io <71457071+heroniewold@users.noreply.github.com>
2021-06-09 14:22:59 +0200

    a12

--- tracking.js
+++ tracking.js
@@ -1,25 +1,21 @@
 navigator.getUserMedia;
 
 const video = document.querySelector('#video');
-const audio = document.querySelector('#audio');
 const canvas = document.querySelector('#canvas');
 const context = canvas.getContext('2d');
 let model;
 
-handTrack.startVideo(video)
-    .then(status => {
-        if(status){
+handTrack.startVideo(video).then(status => {
+        if (status) {
             navigator.getUserMedia(
                 { video: {} }, 
                 stream => {
-                  
-                }{
                     video.srcObject = stream;
             },
             err => console.log(err)
             );
         }
-    })
+    });
 
 handTrack.load().then(lmodel => {
         model = lmodel;
