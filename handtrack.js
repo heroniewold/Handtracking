a53f4f058793262eaedcf2805e530eb53fddb5ff
heroniewold.github.io <71457071+heroniewold@users.noreply.github.com>
2021-06-11 11:51:16 +0200

    o

--- handtrack.js
+++ handtrack.js
@@ -1,5 +1,9 @@
-const video = document.getElementById("#video");
+const video = document.getElementById("myvideo");
 const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+let trackButton = document.getElementById("trackbutton");
+let updateNote = document.getElementById("updatenote");
+
 let isVideo = false;
 let model = null;
 
@@ -14,13 +18,27 @@ function startVideo() {
     handTrack.startVideo(video).then(function (status) {
         console.log("video started", status);
         if (status) {
-           
+            updateNote.innerText = "Video started. Now tracking"
             isVideo = true
-            
-        } 
+            runDetection()
+        } else {
+            updateNote.innerText = "Please enable video"
+        }
     });
 }
-startVideo();
+
+function toggleVideo() {
+    if (!isVideo) {
+        updateNote.innerText = "Starting video"
+        startVideo();
+    } else {
+        updateNote.innerText = "Stopping video"
+        handTrack.stopVideo(video)
+        isVideo = false;
+        updateNote.innerText = "Video stopped"
+    }
+}
+
 
 
 function runDetection() {
@@ -33,7 +51,10 @@ function runDetection() {
     });
 }
 
+// Load the model.
 handTrack.load(modelParams).then(lmodel => {
     // detect objects in the image.
     model = lmodel
+    updateNote.innerText = "Loaded Model!"
+    trackButton.disabled = false
 });
