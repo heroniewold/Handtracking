610aa6b1ffcc7def718873d3ec3b6349fe43e8b0
heroniewold.github.io <71457071+heroniewold@users.noreply.github.com>
2021-06-11 11:36:20 +0200

    poging2

---
+++ handtrack.js
@@ -0,0 +1,37 @@
+const video = document.getElementById("video");
+
+let isVideo = false;
+let model = null;
+
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video  
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.6,    // confidence threshold for predictions.
+}
+
+function startVideo() {
+    handTrack.startVideo(video).then(function (status) {
+        console.log("video started", status);
+        if (status) {
+           
+            isVideo = true
+            runDetection()
+        } 
+    });
+}
+
+function runDetection() {
+    model.detect(video).then(predictions => {
+        console.log("Predictions: ", predictions);
+        model.renderPredictions(predictions, canvas, context, video);
+        if (isVideo) {
+            requestAnimationFrame(runDetection);
+        }
+    });
+}
+
+handTrack.load(modelParams).then(lmodel => {
+    // detect objects in the image.
+    model = lmodel
+});
