22325d1c025689017a3c3cb51826795f85cc8670
heroniewold <71457071+heroniewold@users.noreply.github.com>
2021-06-11 10:53:46 +0200

    isgoed

--- tracking.js
+++ tracking.js
@@ -4,13 +4,14 @@ const defaultParams = {
     flipHorizontal: false,
     outputStride: 16,
     imageScaleFactor: 1,
-    maxNumBoxes: 20,
+    maxNumBoxes: 200,
     iouThreshold: 0.2,
     scoreThreshold: 0.6,
     modelType: "ssd320fpnlite",
     modelSize: "large",
     bboxLineWidth: "2",
     fontSize: 17,
+    videoHeight: 400
 };
 
 const video = document.querySelector('#video');
@@ -32,11 +33,24 @@ handTrack.startVideo(video).then(status => {
     });
 
 function runDetection(){
-    model.detect(video)
-    .then(predictions => {
+    model.detect(video).then(predictions => {
         console.log(predictions);
-    });
-}
+       // model.renderPredictions(predictions, canvas, context, video);
+            if(predictions.length > 0) {
+                console.log("succes david is een ukkie");
+            }
+            if(predictions.length > 1) {
+                console.log("1 is goed, david is 1m10");
+            }
+            if(predictions.length > 2) {
+                console.log("2 is goed, david is 6meter");
+            }
+            if(predictions.length > 3) {
+                console.log("3 is goed, david is 10meter");
+            }
+        
+        })
+    };
 handTrack.load(defaultParams).then(lmodel => {
         model = lmodel;
     });
